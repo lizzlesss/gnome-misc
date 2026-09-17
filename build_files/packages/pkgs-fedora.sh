d630fc37860a7369771c7c96709ee50415c6790f
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-17 19:37:01 -0400

    Update pkgs-fedora.sh

--- build_files/packages/pkgs-fedora.sh
+++ build_files/packages/pkgs-fedora.sh
@@ -19,6 +19,6 @@ dnf copr enable -y bieszczaders/kernel-cachyos-addons
 dnf install -y \
     --enablerepo="copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-addons" \
     --allowerasing \
-    libcap-ng libcap-ng-devel procps-ng procps-ng-devel uksmd libbpf scx-scheds-git scx-tools-git scx-manager
+    libbpf scx-scheds-git scx-tools-git scx-manager
 
 dnf -y copr disable bieszczaders/kernel-cachyos-addons
