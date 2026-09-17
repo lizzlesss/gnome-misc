84093f300bd7b40a1815410f050d08c252e7528a
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-17 14:30:13 -0400

    Update pkgs-fedora.sh

--- build_files/packages/pkgs-fedora.sh
+++ build_files/packages/pkgs-fedora.sh
@@ -9,7 +9,6 @@ dnf install -y \
     intel-lpmd \
     android-tools \
     mangohud \
-    intel-media-driver \
     https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/fedora-44-nbfc-linux-0.5.3-1.x86_64.rpm
 
 # copr
