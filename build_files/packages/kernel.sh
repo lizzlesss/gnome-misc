11d886bc9f1008d393fa81629b5d7dce4d8b47cf
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-17 14:16:53 -0400

    Update kernel.sh

--- build_files/packages/kernel.sh
+++ build_files/packages/kernel.sh
@@ -9,7 +9,7 @@ set -eoux pipefail
     #--allowerasing \
     #libcap-ng libcap-ng-devel bore-sysctl cachyos-ksm-settings procps-ng procps-ng-devel uksmd libbpf scx-scheds-git scx-tools-git scx-manager cachyos-settings ananicy-cpp
 
-dnf copr enable -y catpieleaf/kernel-p03
+dnf copr enable -y bieszczaders/kernel-cachyos-lto
 
 # Remove useless kernels
 readarray -t OLD_KERNELS < <(rpm -qa 'kernel-*')
@@ -22,12 +22,16 @@ fi
 
 # Install kernel packages (noscripts required for 43+)
 dnf install -y \
-    --enablerepo="copr:copr.fedorainfracloud.org:catpieleaf:kernel-p03" \
+    --enablerepo="copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto" \
     --allowerasing \
     --setopt=tsflags=noscripts \
-    kernel-p03 \
-    kernel-p03-core
-KERNEL_VERSION="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-p03)"
+    kernel-cachyos-lto \
+    kernel-cachyos-lto-devel-matched \
+    kernel-cachyos-lto-devel \
+    kernel-cachyos-lto-modules \
+    kernel-cachyos-lto-core
+
+KERNEL_VERSION="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-cachyos-lto)"
 
 # Depmod (required for fedora 43+)
 depmod -a "${KERNEL_VERSION}"
@@ -40,8 +44,8 @@ if [[ -f "${VMLINUZ_SOURCE}" ]]; then
 fi
 
 # Lock kernel packages
-dnf versionlock add "kernel-p03-${KERNEL_VERSION}" || true
-dnf versionlock add "kernel-cachyos-p03-modules-${KERNEL_VERSION}" || true
+dnf versionlock add "kernel-cachyos-lto-${KERNEL_VERSION}" || true
+dnf versionlock add "kernel-cachyos-lto-modules-${KERNEL_VERSION}" || true
 
 
 # Thank you @renner03 for this part
