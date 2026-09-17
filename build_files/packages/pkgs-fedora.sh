5ca7a4c236f0de494ceb20d0d79737efb80ffadc
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-17 14:38:52 -0400

    Update pkgs-fedora.sh

--- build_files/packages/pkgs-fedora.sh
+++ build_files/packages/pkgs-fedora.sh
@@ -6,7 +6,6 @@ dnf remove -y --setopt=install_weak_deps=False \
     gnome-software
 
 dnf install -y \
-    intel-lpmd \
     android-tools \
     mangohud \
     https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/fedora-44-nbfc-linux-0.5.3-1.x86_64.rpm
