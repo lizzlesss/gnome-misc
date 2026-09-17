611efcae9acaec1373bebb4dc7981b28993b8174
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-17 14:57:35 -0400

    Update pkgs-fedora.sh

--- build_files/packages/pkgs-fedora.sh
+++ build_files/packages/pkgs-fedora.sh
@@ -11,9 +11,6 @@ dnf install -y \
     gdm \
     https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/fedora-44-nbfc-linux-0.5.3-1.x86_64.rpm
 
-systemctl enable gdm
-systemctl restart gdm
-
 # copr
 dnf copr enable -y bieszczaders/kernel-cachyos-addons
 
