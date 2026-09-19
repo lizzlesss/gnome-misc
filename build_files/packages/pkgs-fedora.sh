f936f2c0c00b9d48c7afcd0c3a0994956d131afe
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-19 17:57:54 -0400

    Update pkgs-fedora.sh

--- build_files/packages/pkgs-fedora.sh
+++ build_files/packages/pkgs-fedora.sh
@@ -3,7 +3,9 @@
 set -eoux pipefail
 
 dnf remove -y \
-    firefox
+    firefox \
+    tuned \
+    tuned-ppd
 
 dnf remove -y --setopt=install_weak_deps=False \
     gnome-software \
@@ -21,6 +23,7 @@ dnf install -y \
     android-tools \
     mangohud \
     NetworkManager-tui \
+    power-profiles-daemon \
     testdisk \
     qphotorec \
     https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/fedora-44-nbfc-linux-0.5.3-1.x86_64.rpm
