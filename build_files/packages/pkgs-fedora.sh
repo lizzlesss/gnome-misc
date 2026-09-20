96225d3b88e3cdf2c61572847fea458549e77bba
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-20 12:31:29 -0400

    Update pkgs-fedora.sh

--- build_files/packages/pkgs-fedora.sh
+++ build_files/packages/pkgs-fedora.sh
@@ -24,6 +24,7 @@ dnf install -y \
     intel-media-driver \
     mangohud \
     NetworkManager-tui \
+    starship \
     testdisk \
     qphotorec \
     https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/fedora-44-nbfc-linux-0.5.3-1.x86_64.rpm
