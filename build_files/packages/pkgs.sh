748ae019f285343753a2e133991ccbe3e8a64db9
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-20 10:32:24 -0400

    Update pkgs.sh

--- build_files/packages/pkgs.sh
+++ build_files/packages/pkgs.sh
@@ -7,11 +7,14 @@ pacman -R --noconfirm power-profiles-daemon
 pacman -Sy --noconfirm \
     intel-lpmd \
     intel-media-driver \
+    linux-firmware-intel \
     scx-scheds \
     scx-tools \
     starship \
     tuned \
     tuned-ppd \
+    vpl-gpu-rt \
+    vulkan-intel \
     wget
 
 wget https://github.com/nbfc-linux/nbfc-linux/releases/download/0.5.3/arch-linux-nbfc-linux-git-0.5.3-1-x86_64.pkg.tar.zst
