a57f1dd15d97e4d0cba0a00e74e6c323ee546779
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-20 09:37:40 -0400

    intel-media-driver and ffmpeg-full

--- build_files/packages/pkgs.sh
+++ build_files/packages/pkgs.sh
@@ -6,7 +6,9 @@ pacman -Syu --noconfirm
 pacman -R --noconfirm power-profiles-daemon
 pacman -Sy --noconfirm \
     fish \
+    ffmpeg-full \
     intel-lpmd \
+    intel-media-driver \
     scx-scheds \
     scx-tools \
     starship \
