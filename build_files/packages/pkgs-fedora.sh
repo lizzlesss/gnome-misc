858a49ce7e0ccd9dd9f5263f5b98db5e70f10865
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-20 13:12:30 -0400

    Update pkgs-fedora.sh

--- build_files/packages/pkgs-fedora.sh
+++ build_files/packages/pkgs-fedora.sh
@@ -10,16 +10,19 @@ dnf remove -y --setopt=install_weak_deps=False \
     gnome-tour \
     yelp
 
-#dnf install -y \
-    #https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
-    #https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-#dnf swap -y ffmpeg-free ffmpeg --allowerasing
-#dnf install -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin --skip-broken
+dnf swap -y ffmpeg-free ffmpeg --allowerasing
+dnf install -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin --skip-broken
 
 dnf install -y \
     android-tools \
+    distrobox \
     fish \
+    intel-media-driver \
+    intel-vpl-gpu-rt \
     mangohud \
     NetworkManager-tui \
     testdisk \
