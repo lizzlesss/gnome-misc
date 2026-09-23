b2f618424e3cd14cb12989f7e830931ef3e30a7f
casey / liz <63914304+lizzlesss@users.noreply.github.com>
2026-09-23 13:56:54 -0400

    Update kernel.sh

--- build_files/packages/kernel.sh
+++ build_files/packages/kernel.sh
@@ -9,7 +9,7 @@ set -eoux pipefail
     #--allowerasing \
     #libcap-ng libcap-ng-devel bore-sysctl cachyos-ksm-settings procps-ng procps-ng-devel uksmd libbpf scx-scheds-git scx-tools-git scx-manager cachyos-settings ananicy-cpp
 
-dnf copr enable -y bieszczaders/kernel-cachyos-lto
+dnf copr enable -y catpieleaf/kernel-p03
 
 # Remove useless kernels
 readarray -t OLD_KERNELS < <(rpm -qa 'kernel-*')
@@ -22,16 +22,12 @@ fi
 
 # Install kernel packages (noscripts required for 43+)
 dnf install -y \
-    --enablerepo="copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos-lto" \
+    --enablerepo="copr:copr.fedorainfracloud.org:catpieleaf:kernel-p03" \
     --allowerasing \
     --setopt=tsflags=noscripts \
-    kernel-cachyos-lto \
-    kernel-cachyos-lto-devel-matched \
-    kernel-cachyos-lto-devel \
-    kernel-cachyos-lto-modules \
-    kernel-cachyos-lto-core
-
-KERNEL_VERSION="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-cachyos-lto)"
+    kernel-p03 \
+    kernel-p03-core
+KERNEL_VERSION="$(rpm -q --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' kernel-p03)"
 
 # Depmod (required for fedora 43+)
 depmod -a "${KERNEL_VERSION}"
@@ -44,32 +40,8 @@ if [[ -f "${VMLINUZ_SOURCE}" ]]; then
 fi
 
 # Lock kernel packages
-dnf versionlock add "kernel-cachyos-lto-${KERNEL_VERSION}" || true
-dnf versionlock add "kernel-cachyos-lto-modules-${KERNEL_VERSION}" || true
-
-
-# Thank you @renner03 for this part
-export DRACUT_NO_XATTR=1
-dracut --force \
-  --no-hostonly \
-  --kver "${KERNEL_VERSION}" \
-  --add-drivers "btrfs nvme xfs ext4" \
-  --reproducible -v --add ostree \
-  -f "/usr/lib/modules/${KERNEL_VERSION}/initramfs.img"
-
-chmod 0600 "/lib/modules/${KERNEL_VERSION}/initramfs.img"
-depmod -a "${KERNEL_VERSION}"
-
-# Copy vmlinuz
-VMLINUZ_SOURCE="/usr/lib/kernel/vmlinuz-${KERNEL_VERSION}"
-VMLINUZ_TARGET="/usr/lib/modules/${KERNEL_VERSION}/vmlinuz"
-if [[ -f "${VMLINUZ_SOURCE}" ]]; then
-    cp "${VMLINUZ_SOURCE}" "${VMLINUZ_TARGET}"
-fi
-
-# Lock kernel packages
-dnf versionlock add "kernel-cachyos-lto-${KERNEL_VERSION}" || true
-dnf versionlock add "kernel-cachyos-lto-modules-${KERNEL_VERSION}" || true
+dnf versionlock add "kernel-p03-${KERNEL_VERSION}" || true
+dnf versionlock add "kernel-cachyos-p03-modules-${KERNEL_VERSION}" || true
 
 
 # Thank you @renner03 for this part
